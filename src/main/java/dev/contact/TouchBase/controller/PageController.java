470bce1c2b2aa8630ceeb6fae65873e0bd8e500e
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-12-04 00:29:02 +0530

    Basic html plus setting up jpa

--- src/main/java/dev/contact/TouchBase/controller/PageController.java
+++ src/main/java/dev/contact/TouchBase/controller/PageController.java
@@ -1,15 +1,17 @@
 package dev.contact.TouchBase.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class PageController {
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         String message = "Welcome to TouchBase home";
+        System.out.println(message);
+        model.addAttribute("name", "Welcome to Touch Base");
+        model.addAttribute("githubRepo", "https://github.com/ankit-tiwari25/TouchBase/tree/master");
         return "home";
     }
 }
